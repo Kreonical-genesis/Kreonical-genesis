74f22c479a3263d9950f9d942bac8055d85f6a52
Kreonical-genesis <kreogen20066@gmail.com>
2025-08-02 13:09:49 +0300

    новая главная страница с сылками

--- js/page.js
+++ js/page.js
@@ -7,3 +7,39 @@ document.querySelectorAll('.toggle-desc').forEach(btn => {
     btn.textContent = expanded ? 'Показать описание' : 'Скрыть описание';
   });
 });
+const images = {
+  'bg-builds': [
+    'assets/images/build1.jpg',
+    'assets/images/build2.jpg',
+    'assets/images/build3.jpg'
+  ],
+  'bg-rp': [
+    'assets/images/rp1.jpg',
+    'assets/images/rp2.jpg',
+    'assets/images/rp3.jpg'
+  ],
+  'bg-models': [
+    'assets/images/model1.jpg',
+    'assets/images/model2.jpg',
+    'assets/images/model3.jpg'
+  ],
+  'bg-code': [
+    'assets/images/code1.jpg',
+    'assets/images/code2.jpg',
+    'assets/images/code3.jpg'
+  ]
+};
+
+const bodyClass = document.body.classList[0];
+const slideshowContainer = document.querySelector('.background-slideshow');
+const bgList = images[bodyClass] || [];
+let index = 0;
+
+if (slideshowContainer && bgList.length) {
+  slideshowContainer.style.backgroundImage = `url('${bgList[index]}')`;
+
+  setInterval(() => {
+    index = (index + 1) % bgList.length;
+    slideshowContainer.style.backgroundImage = `url('${bgList[index]}')`;
+  }, 8000); // смена каждые 8 секунд
+}
